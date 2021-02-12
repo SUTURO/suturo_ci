d1f53d0c44c3ea2ef3a2864f8c7f6936a3dfd819
Paul Schnipper <51263940+Schnippa@users.noreply.github.com>
2021-02-12 16:45:14 +0100

    Update build_suturo_resources.sh

--- entrypoints/build_suturo_resources.sh
+++ entrypoints/build_suturo_resources.sh
@@ -5,6 +5,6 @@ mv resources resources_ws/src/
 
 source /opt/ros/melodic/setup.bash
 
-cd resources_ws && catkin build
+cd resources_ws && catkin build 2> error && test -s error && exit 1
 
 echo "Building SUTURO Resources Successfull"

e636a1f93265561490758825173dca6abaa03459
Paul Schnipper <51263940+Schnippa@users.noreply.github.com>
2021-02-13 17:12:52 +0100

    Update build_suturo_resources.sh

--- entrypoints/build_suturo_resources.sh
+++ entrypoints/build_suturo_resources.sh
@@ -8,7 +8,7 @@ source /opt/ros/melodic/setup.bash
 cd resources_ws/src
 
 wstool init
-wstool merge https://raw.githubusercontent.com/SUTURO/suturo_resources/ci_test/rosinstall.rosinstall
+wstool merge https://raw.githubusercontent.com/SUTURO/suturo_resources/ci_test/rosinstall.rosinstall -y
 wstool update
 
 cd ..
